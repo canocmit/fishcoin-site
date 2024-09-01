32effcafc949180becbd53692e978387bdb82a10
prorum <179600312+prorum@users.noreply.github.com>
2024-09-01 18:25:26 +0700

    feat: finish template

---
+++ src/constant/contract.ts
@@ -0,0 +1,24 @@
+export const CONTRACT = {
+  name: 'BabyBaby',
+  symbol: 'BYBY',
+  ca: '0x0000000000000000000000000000000000000000',
+  totalSupply: 1000000000,
+  taxPercentage: 5,
+  ownershipRenounced: true,
+  description:
+    'BabyBaby is a community made and owned Crypto currency project. We are aspiring to become the new high quality standard within the Crypto Space. A group of like minded individuals have come together to create the new golden standard of what a community should have to offer. We have a 5% tax on both buy and sell orders. We are in no way affiliated with Baby or the Baby family. However, we will be donating 5% of our marketing budget to the Baby campaign.',
+  partners: [
+    {
+      name: 'PinkSale',
+      url: 'https://www.pinksale.finance',
+      imgUrl: '/pinksale.png',
+    },
+    { name: 'DEXView', url: 'https://www.dexview.com', imgUrl: '/dexview.svg' },
+  ],
+  telegram: 'https://t.me/UnKnOwNcHanNeL',
+  twitter: 'https://twitter.com/UnKnOwNcHanNeL',
+  discord: 'https://discord.com/UnKnOwNcHanNeL',
+  logo1: '/logo.png',
+  logo2: '/logo.png',
+  logo3: '/logo.png',
+};
